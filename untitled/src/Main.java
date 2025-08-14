4629bde53c9b504640fa6eed876bd12cdbb6cf66
galina <galina.krupitsa@gmail.com>
2025-08-14 05:07:10 -0700

    df

---
+++ untitled/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Иван", 8);
+        person1.Kotik();
+        person1.displayInfo();
+
+
+    }
+}
