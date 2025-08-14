95fa2160fb0e2d101b6c8c2e2a78ba95d54b33ee
galina <galina.krupitsa@gmail.com>
2025-08-14 05:32:36 -0700

    df

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -7,6 +8,16 @@ public class Main {
         person1.Kotik();
         person1.displayInfo();
 
-
+        Class<?> clazz = Person.class;
+        System.out.println("Поля класса:");
+Field[] fields = clazz.getDeclaredFields();
+for (Field field : fields) {
+    System.out.println(field.getName());
+}
+System.out.println("Методы класса:");
+Method[] methods = clazz.getDeclaredMethods();
+for (Method method : methods) {
+    System.out.println(method.getName());
+}
     }
 }
