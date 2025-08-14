2bc5477ddea8f65161792c1715765fa3740e4b6d
galina <galina.krupitsa@gmail.com>
2025-08-14 05:06:57 -0700

    df

---
+++ untitled/src/Person.java
@@ -0,0 +1,18 @@
+public class Person {
+    String name;
+    int age;
+
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+    public void displayInfo() {
+        System.out.println("Имя: " + name + ", возраст: " + age);
+    }
+    public void Kotik() {
+        System.out.println("Котики милые");
+    }
+}
